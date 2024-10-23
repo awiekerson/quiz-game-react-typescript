37796e4250a2525790a3e65b049d83877b607ba4
awiekerson <77976466+awiekerson@users.noreply.github.com>
2024-10-23 14:30:54 -0500

    Added API/Error catching and question/answers visible to the user

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,65 @@
 
 import './App.scss'
+import { useEffect } from 'react';
 import Score from './components/Score.tsx';
 import Game from './components/Game.tsx';
+import { useQuiz, Question, QuestionsResponse} from './QuizContext.tsx';
+import FullPageLoader from "./components/FullPageLoader.tsx"
 
 function App() {
 
+  const {state, dispatch} = useQuiz();
+
+async function fetchQuestion() {
+
+  try {
+    dispatch({type: "setStatus", payload: "fetching"});
+    const response = await fetch('https://opentdb.com/api.php?amount=1');
+    let data: QuestionsResponse = await(response.json());
+
+    if (data.response_code === 0) {
+      let question : Question = data.results[0];
+      dispatch({type: "setStatus", payload: "ready"});
+      dispatch({type: "setQuestion", payload: question});
+
+    } else {
+      dispatch({type: "setStatus", payload: "error"});
+    }
+
+  } catch (err){
+    console.log(err);
+    dispatch({type: "setStatus", payload: "error"});
+
+  }
+
+  
+  
+}
+
+
+  useEffect(() => {
+    if(state.gameStatus == "idle") {
+      fetchQuestion();
+    }
+  });
+  //Fired anytime there is a change in the state and once at the beginning
 
   return (
     <>
-      <Score />
-      <Game /> 
-       
+    { state.gameStatus == 'fetching' ?  
+    <FullPageLoader/>
+     : state.gameStatus == "error" ? 
+     <p>Error...</p>
+
+     : state.gameStatus == "ready" ?  
+      <>
+        <Score />
+        <Game /> 
+      </> 
+     :
+      ''
+    }
+
     </>
   )
 }
---
+++ src/QuizContext.tsx
@@ -0,0 +1,69 @@
+import { createContext, useContext, useReducer } from 'react';
+
+
+export type Status = "idle" | "fetching" | "ready" | "error";
+
+export interface Question {
+    category: string;
+    type: 'multiple' | 'boolean';
+    difficulty: 'easy' | 'medium' | 'hard';
+    question: string;
+    correct_answer: string;
+    incorrect_answers: string[];
+}
+
+export interface QuestionsResponse {
+    response_code: number;
+    results: Question[];
+}
+
+interface QuizContext {
+    state: QuizState, 
+    dispatch: React.Dispatch<QuizAction>
+}
+
+interface QuizState {
+    gameStatus: Status,
+    question: Question | null
+}
+
+const initialState: QuizState = {
+    gameStatus: "idle",
+    question: null
+}
+
+type QuizAction = { type: "setStatus"; payload: Status } | 
+{type: "setQuestion"; payload: Question}
+
+
+const QuizContext = createContext<QuizContext>({
+    state: initialState, 
+    dispatch: () => null 
+});
+
+export function QuizProvider({children} : {children : React.ReactElement}) {
+    const [state, dispatch] = useReducer(QuizReducer, initialState);
+
+    return (
+        <QuizContext.Provider value={{state, dispatch}}>
+            {children}
+        </QuizContext.Provider>
+    )
+}
+
+export function useQuiz() {
+    return useContext(QuizContext);
+}
+
+function QuizReducer(state: QuizState, action: QuizAction): QuizState {
+    switch (action.type) {
+      case "setStatus":
+        return {...state, gameStatus: action.payload};
+      case "setQuestion":
+        return {...state, question: action.payload};
+     
+        default:
+        throw new Error("Unknown action");
+    }
+  }
+  
